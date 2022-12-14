144607dbfd123cf83be2cb9963c7f9bbb3d72739
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-12-14 10:59:36 +0000

    Add Genre/Item

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
-
-resources :items, only: [:index, :new, :create, :show, :edit, :update]
-resources :genres, only: [:index, :create, :edit, :update]
-
+namespace :admin do
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :genres, only: [:index, :create, :edit, :update]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
