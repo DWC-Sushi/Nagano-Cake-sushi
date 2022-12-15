6e4c3bd5984c0bf335d93f1dcbde38738389eb0c
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-12-15 04:30:16 +0000

    Genre更新

--- config/routes.rb
+++ config/routes.rb
@@ -38,5 +38,5 @@ namespace :admin do
   resources :items, only: [:index, :new, :create, :show, :edit, :update]
   resources :genres, only: [:index, :create, :edit, :update]
 end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlend
+  
