388e489bdf8cf32fd58b01a992c48c2730c4dbe4
EC2 Default User <117144349+ryo59124@users.noreply.github.com>
2022-12-15 04:29:52 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,40 @@
+
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
+  
+ namespace :public do
+    get 'orders/new'
+    get 'orders/index'
     get 'orders/show'
-    get 'orders/update'
+    get 'orders/complete'
+    post 'orders/confirm'
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
+  
   # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+  resources :cart_items, only: [:index, :create, :update, :destroy]
+  delete 'cart_items' => 'cart_items#destroy_all', as: 'destroy_all'
+
 
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
+
+  namespace :admin do
+    get 'homes/top'
+  end
+  
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :order_details, only: [:update]
+    resources :orders, only: [:show, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
