2ce877323a9ea282e39c21e55233191396f73a33
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2022-12-16 05:43:57 +0000

    cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,13 @@
 class Public::CartItemsController < ApplicationController
-  
-  # カート内一覧
+
+  # カートの中の一覧
   def index
-    @cart_items = current_custmer.cart_items
+    @cart_items = current_customer.cart_items.all
     @total_price = @cart_items.sum{|cart_item|cart_item.item.price_without_tax * cart_item.amonut * 1.1}
   end
-  
-  # カート内に商品を追加
+
+  # カートの中にに商品を追加
   def create
-    # @cart_item = current_customer.cart_items.build(item_id: params[:item_id])
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.item_id = params[:item_id]
@@ -21,17 +20,32 @@ class Public::CartItemsController < ApplicationController
       render "customers/items/show"
     end
   end
-  
+
   # 削除,個数の変更時、カート商品を再計算する
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to customers_cart_items_path
   end
-  
+
+  # カートの中の商品を一つだけ削除する
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    flash.now[:alert] = "#{@cart_item.item.name}を削除しました"
+    redirect_to customers_cart_items_path
   end
-  
+
+  # カートの中を全て削除する
   def destroy_all
+    @cart_item = current_customer.cart_items
+    @cart_item.destroy_all
+    flash[:alert] = "カートの商品を全て削除しました"
+    redirect_to customers_cart_items_path
   end
+
+  private
+ def item_params
+  params.require(:cart_items).permit(:item_id,:customer_id,:amount,:image_id,:price)
+ end
 end
