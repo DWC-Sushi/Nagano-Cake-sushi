9948343fff16f910fad39e871e2d342b847130a5
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2022-12-16 06:09:17 +0000

    cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,37 +1,50 @@
 class Public::CartItemsController < ApplicationController
-  
+  before_action :authenticate_customer!
   # カート内一覧
   def index
-    @cart_items = current_custmer.cart_items
-    @total_price = @cart_items.sum{|cart_item|cart_item.item.price_without_tax * cart_item.amonut * 1.1}
+    @cart_items = current_member.cart_items.all
   end
-  
-  # カート内に商品を追加
-  def create
-    # @cart_item = current_customer.cart_items.build(item_id: params[:item_id])
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.item_id = params[:item_id]
 
-    if @cart_item.save
-      flash[:notice] = "#{@cart_item.item.name}をカートに追加しました。"
-      redirect_to customers_cart_items_path
+  # カート内に商品追加
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_member.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.quantity += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+      @cart_items = current_customer.cart_items.all
+      render 'index'
     else
-      flash[:alert] = "個数を選択してください"
-      render "customers/items/show"
+      render 'index'
     end
   end
-  
-  # 削除,個数の変更時、カート商品を再計算する
+
+  # カート内更新
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
-    redirect_to customers_cart_items_path
+    @cart_items = CartItem.all
+    redirect_to cart_items_path
   end
-  
+
+  # カート内の商品１つのみ削除
   def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    @cart_items = CartItem.all
+    render 'index'
   end
-  
+# カート内全て削除
   def destroy_all
+    cart_items = CartItem.all
+    cart_items.destroy_all
+    render 'index'
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :price, :amount)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,13 @@ Rails.application.routes.draw do
     post 'orders/confirm'
   end
 
+  namespace :public do
+    resources :cart_items, only: [:index, :update, :destroy, :create] do
+    collection do
+      delete 'destroy_all'
+    end
+  end
+
   # 顧客用
 
 # URL /customers/sign_in ...
@@ -31,10 +38,8 @@ devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  namespace :public do
-    resources :cart_items, only: [:index, :create, :update, :destroy]
-    delete 'cart_items' => 'cart_items#destroy_all', as: 'destroy_all'
-  end
+
+
 
 # 管理者用
 # URL /admin/sign_in ...
