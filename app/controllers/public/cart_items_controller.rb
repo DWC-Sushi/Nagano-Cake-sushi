77741ff476187ca8c32310964000479505cd841a
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2022-12-17 02:45:30 +0000

    orders4

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,16 @@
 class Public::CartItemsController < ApplicationController
-  before_action :authenticate_customer!
+  # before_action :authenticate_customer!
   # カート内一覧
   def index
-    @cart_items = current_member.cart_items.all
+    @cart_items = current_customer.cart_items.all
   end
 
   # カート内に商品追加
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-      cart_item = current_member.cart_items.find_by(item_id: params[:cart_item][:item_id])
-      cart_item.quantity += params[:cart_item][:amount].to_i
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
       cart_item.save
       redirect_to cart_items_path
     elsif @cart_item.save
