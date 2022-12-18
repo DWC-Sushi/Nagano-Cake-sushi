f297a99dc7b0f7d43671860ba8175aab702bf4ec
EC2 Default User <117144349+ryo59124@users.noreply.github.com>
2022-12-18 13:45:55 +0000

    view修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -7,9 +7,9 @@ before_action :authenticate_admin!
     @customer = @order.customer
     @order_details = Order_detail.all
     if @order.update(order_params)
-      redirect_to admin_order_detail_path(@order), notice: "You have updated order successfully."
+      redirect_to admin_order_path(@order), notice: "You have updated order successfully."
     else
-      render :??????
+      render :edit_admin_customer_path
     end
   end
 end
