9aeaad13e16093827f51920ab4b57ad7d082672d
EC2 Default User <117144349+ryo59124@users.noreply.github.com>
2022-12-19 05:17:58 +0000

    4

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -27,7 +27,7 @@ before_action :authenticate_admin!
   private
 
   def customer_params
-    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_delated)
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
   end
 
 
