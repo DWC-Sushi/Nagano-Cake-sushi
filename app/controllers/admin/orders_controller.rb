fe7d811c871d22bfee4fea17356829dbe5e9c5df
EC2 Default User <117144349+ryo59124@users.noreply.github.com>
2022-12-19 09:46:08 +0000

    注文ステータス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,30 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order_details = OrderDetail.where(order_id: params[:id])
+    @order = Order.find(params[:id])
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+     if @order.update(order_params)
+        @order.order_details.update_all(status: 1)
+       redirect_to request.referer
+     else
+       redirect_to request.referer
+     end
   end
+
+
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to request.referer
+  end
+  
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
   
-  enum order_status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
+  enum status: { wait_payment: 0, confirm_payment: 1, making: 2, preparing_ship: 3, finish_prepare: 4 }
   enum payment_method: { credit_card: 0, transfer: 1 }
 end
