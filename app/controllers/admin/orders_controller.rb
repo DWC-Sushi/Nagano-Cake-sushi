481a9b5d3e5b34c50eb2f58ebc126399690cad8e
EC2 Default User <117144349+ryo59124@users.noreply.github.com>
2022-12-19 13:44:15 +0000

    注文一覧修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -21,4 +21,8 @@ class Admin::OrdersController < ApplicationController
     params.require(:order).permit(:status)
   end
   
+  def order_detail_params
+    params.require(:order_detail).permit(:item)
+  end
+  
 end
